9d95c0a1e1e216be8a829f1731dd67568acefcc1
Gregory Giecold <GGiecold@users.noreply.github.com>
2017-06-15 00:47:34 -0400

    Create setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
     
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README'), encoding = 'utf-8') as f:
+with open(path.join(here, 'README.md'), encoding = 'utf-8') as f:
     long_description = f.read()
     
 
